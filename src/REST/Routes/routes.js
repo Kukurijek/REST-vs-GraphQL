5ca071f31991a416c7cf5577cef2c380acb61778
Alexander Niklasson <alexander.niklasson@live.com>
2023-04-14 15:12:16 +0200

    Test for routes

--- src/REST/Routes/routes.js
+++ src/REST/Routes/routes.js
@@ -1,11 +1,27 @@
 'use strict';
-
+var counter = 0;
 const express = require('express');
 const api = express.Router();
 
 api.get('/', (req, res) => {
-    res.send({
-        message: 'Hello World!'
-    });
+    counter++;
+    if (counter > 100000) {
+
+    } else {
+        res.send({
+            message: test()
+        });
+    }
+
 });
 module.exports = api;
+
+function test() {
+    let arr;
+    for (let i = 0; i < 1000; i++) {
+        arr[i] = i;
+    }
+
+
+    return arr;
+}
